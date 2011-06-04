ed2787dcec575b216e7dda3223d1f895155d9f32
Joshua Essex <essexjosh@gmail.com>
2011-06-04 17:08:30 -0400

    Altered caching hierarchy to leave room for other potential caches or cache implementations by adding a generic interface for JsonCache

--- src/com/jessex/gistacious/json/JsonCache.java
+++ src/com/jessex/gistacious/json/JsonCache.java
@@ -1,54 +1,13 @@
 package com.jessex.gistacious.json;
 
-import java.util.HashMap;
+public interface JsonCache<K,V> {
 
-import com.jessex.gistacious.gist.GistUser;
-
-public class JsonCache {
-
-	private HashMap<String, GistUser> userCache;
+	void resetCache();
 	
-	public JsonCache() {
-		this.userCache = new HashMap<String, GistUser>();
-	}
+	boolean isInCache(K key);
 	
-	/**
-	 * Returns whether or not the GistUser with the given login is in the user
-	 * cache.
-	 * @param login -
-	 * 			login of GistUser
-	 * @return isInCache -
-	 * 			whether or not user with login is in cache
-	 */
-	public boolean isInCache(String login) {
-		return userCache.containsKey(login);
-	}
+	V getValue(K key);
 	
-	/**
-	 * Returns the GistUser in the user cache with the given login.
-	 * @param login -
-	 * 			login of GistUser
-	 * @return user -
-	 * 			GistUser with given login
-	 */
-	public GistUser getUser(String login) {
-		return userCache.get(login);
-	}
-	
-	/**
-	 * Adds the given GistUser to the user cache.
-	 * @param user -
-	 * 			GistUser to add to cache
-	 */
-	public void addUser(GistUser user) {
-		userCache.put(user.getLogin(), user);
-	}
-	
-	/**
-	 * Resets the user cache by clearing all entries.
-	 */
-	public void resetCache() {
-		this.userCache.clear();
-	}
+	void addValue(K key, V value);
 	
 }
