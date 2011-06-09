df72e131f8cde620ac588183fc17392f9415c799
Joshua Essex <essexjosh@gmail.com>
2011-06-09 16:37:59 -0400

    Added Javadocs to interface

--- src/com/jessex/gistacious/json/JsonDeserializer.java
+++ src/com/jessex/gistacious/json/JsonDeserializer.java
@@ -2,10 +2,28 @@ package com.jessex.gistacious.json;
 
 public interface JsonDeserializer {
 
+	/**
+	 * Parses a Gist object from the provided JSON text.
+	 * @param json -
+	 * 			JSON text to parse
+	 * @return Gist object
+	 */
 	Object deserializeGistFromJson(String json);
 	
+	/**
+	 * Parses a GistComment object from the provided JSON text.
+	 * @param json -
+	 * 			JSON text to parse
+	 * @return GistComment object
+	 */
 	Object deserializeCommentFromJson(String json);
 	
+	/**
+	 * Parses a GistUser object from the provided JSON text.
+	 * @param json -
+	 * 			JSON text to parse
+	 * @return GistUser object
+	 */
 	Object deserializeUserFromJson(String json);
 	
 }
