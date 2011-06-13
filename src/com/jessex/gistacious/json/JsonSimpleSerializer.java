d76e863adbfe9a43a3d91327332a9bfecf8a7102
Joshua Essex <essexjosh@gmail.com>
2011-06-13 17:11:44 -0400

    Added shells for new methods to be implemented

--- src/com/jessex/gistacious/json/JsonSimpleSerializer.java
+++ src/com/jessex/gistacious/json/JsonSimpleSerializer.java
@@ -1,5 +1,41 @@
 package com.jessex.gistacious.json;
 
+import java.util.List;
+
+import com.jessex.gistacious.gist.Gist;
+import com.jessex.gistacious.gist.GistComment;
+import com.jessex.gistacious.gist.GistUser;
+
 public class JsonSimpleSerializer implements JsonSerializer {
+
+	@Override
+	public String serializeJsonFromGist(Gist gist) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String serializeJsonFromGists(List<Gist> gists) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String serializeJsonFromGistComment(GistComment comment) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String serializeJsonFromGistComments(List<GistComment> comments) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String serializeJsonFromUser(GistUser user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
