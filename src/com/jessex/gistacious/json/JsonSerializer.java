8bdb0b52ac1249dfceb845903e054bd07c264336
Joshua Essex <essexjosh@gmail.com>
2011-06-13 17:10:40 -0400

    Added methods into interface

--- src/com/jessex/gistacious/json/JsonSerializer.java
+++ src/com/jessex/gistacious/json/JsonSerializer.java
@@ -1,5 +1,19 @@
 package com.jessex.gistacious.json;
 
+import java.util.List;
+
+import com.jessex.gistacious.gist.*;
+
 public interface JsonSerializer {
 
+	String serializeJsonFromGist(Gist gist);
+	
+	String serializeJsonFromGists(List<Gist> gists);
+	
+	String serializeJsonFromGistComment(GistComment comment);
+	
+	String serializeJsonFromGistComments(List<GistComment> comments);
+	
+	String serializeJsonFromUser(GistUser user);
+	
 }
