1fe75967a03ebb29e233272ea45e5cb70a1ddf40
Joshua Essex <essexjosh@gmail.com>
2011-06-12 21:27:37 -0400

    Changed return types of methods in interface

--- src/com/jessex/gistacious/json/JsonDeserializer.java
+++ src/com/jessex/gistacious/json/JsonDeserializer.java
@@ -1,5 +1,8 @@
 package com.jessex.gistacious.json;
 
+import java.util.List;
+import com.jessex.gistacious.gist.*;
+
 public interface JsonDeserializer {
 
 	/**
@@ -8,7 +11,16 @@ public interface JsonDeserializer {
 	 * 			JSON text to parse
 	 * @return Gist object
 	 */
-	Object deserializeGistFromJson(String json);
+	Gist deserializeGistFromJson(String json);
+	
+	/**
+	 * Parses a list of Gist objects from the provided JSON text.
+	 * @param json -
+	 * 			JSON text to parse
+	 * @return gists -
+	 * 			List of Gist objects
+	 */
+	List<Gist> deserializeGistsFromJson(String json);
 	
 	/**
 	 * Parses a GistComment object from the provided JSON text.
@@ -16,7 +28,16 @@ public interface JsonDeserializer {
 	 * 			JSON text to parse
 	 * @return GistComment object
 	 */
-	Object deserializeCommentFromJson(String json);
+	GistComment deserializeCommentFromJson(String json);
+	
+	/**
+	 * Parses a list of GistComment objects from the provided JSON text.
+	 * @param json -
+	 * 			JSON text to parse
+	 * @return gistComments -
+	 * 			List of GistComment objects
+	 */
+	List<GistComment> deserializeCommentsFromJson(String json);
 	
 	/**
 	 * Parses a GistUser object from the provided JSON text.
@@ -24,6 +45,6 @@ public interface JsonDeserializer {
 	 * 			JSON text to parse
 	 * @return GistUser object
 	 */
-	Object deserializeUserFromJson(String json);
+	GistUser deserializeUserFromJson(String json);
 	
 }
